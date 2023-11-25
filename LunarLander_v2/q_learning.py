0bdcbd4c451a8fcfd684335115f0fd110c3e0eb7
tikhon <tikhon.radk@gmail.com>
2023-11-25 21:52:38 +0300

    q-learning realisation created

--- LunarLander_v2/q_learning.py
+++ LunarLander_v2/q_learning.py
@@ -3,11 +3,17 @@ import gym
 
 
 env_id = "LunarLander-v2"
-env = gym.make(env_id, render_mode="human")
+env = gym.make(env_id, render_mode="rgb_array")
 
 bin_size = [10] * len(env.observation_space.high)
 bin_win = (env.observation_space.high - env.observation_space.low + 1) / bin_size
 q_table = np.random.uniform(low=0, high=1, size=(bin_size + [env.action_space.n]))
+env.close()
+
+memory_usage_mb = q_table.nbytes / (1024 * 1024)
+print(f"Q-table size: {q_table.shape}")
+print(f"Q-table memory usage: {memory_usage_mb:.2f} MB")
+
 
 def get_discrete_state(state):
     discrete_state = (state - env.observation_space.low) / bin_win
@@ -27,10 +33,12 @@ for episode in range(1, episodes):
     state, _ = env.reset()
     discrete_state = get_discrete_state(state)
 
+    sum_rewards = 0
     try:
         while not (terminated or truncated):
             action = np.argmax(q_table[discrete_state])
             new_state, reward, terminated, truncated, info = env.step(action)
+            sum_rewards += reward
 
             new_discrete_state = get_discrete_state(new_state)
             max_future_q = np.max(q_table[new_discrete_state])
@@ -41,12 +49,10 @@ for episode in range(1, episodes):
 
             discrete_state = new_discrete_state
 
-            if terminated or truncated:
-                q_table[discrete_state][action] = 0
     except IndexError:
-        print("IndexError")
+        print("Lunar Module out of bounds.")
 
     if episode % show_every == 0:
-        print(f"Episode {episode} finished after {env._elapsed_steps} timesteps.")
+        print(f"Episode {episode} finished after {env._elapsed_steps} timesteps. Reward: {sum_rewards:.2f}")
 
-env.close()
+    env.close()
